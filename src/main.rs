19ea87ff24e75e2aec66972920b2f047b45da2bc
Nigth <omnes.theo@gmail.com>
2022-11-03 21:03:43 +0100

    first ping to server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpStream;
+
 fn main() {
     println!("Hello, world!");
+
+    if let Ok(stream) = TcpStream::connect("127.0.0.1:7878") {
+        println!("Connected to the server!");
+    } else {
+        println!("Couldn't connect to server...");
+    }
 }
