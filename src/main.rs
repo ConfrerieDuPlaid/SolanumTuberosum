4e45848ac5a2a976fa736c23f7804c628576edf4
Nigth <omnes.theo@gmail.com>
2022-11-03 21:48:36 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
+use std::io;
+use std::io::Write;
 use std::net::TcpStream;
+use byteorder::{ByteOrder, BigEndian};
 
-fn main() {
+fn main() -> io::Result<()> {
     println!("Hello, world!");
 
-    if let Ok(stream) = TcpStream::connect("127.0.0.1:7878") {
+    if let Ok(mut stream) = TcpStream::connect("127.0.0.1:7878") {
         println!("Connected to the server!");
+
+        let msg = "Hello".to_string();
+        let size = msg.len();
+
+        BigEndian::write_u32();
+
+        stream.write(b"5 Hello")?;
+
+        stream.flush()?;
+        Ok(())
     } else {
         println!("Couldn't connect to server...");
+        Ok(())
     }
 }
