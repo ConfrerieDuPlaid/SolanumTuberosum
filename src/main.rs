31309f12ed8ebe01cd9062bb9f7620f8e88e254b
Nathan-dev-dot <maerna1@gmail.com>
2022-11-16 19:54:57 +0100

    Send hello message and read response from stream

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,56 @@
-use std::io;
+
 use std::io::{Read, Write};
 use std::net::TcpStream;
+use serde::{Deserialize, Serialize};
 
-fn main() -> io::Result<()> {
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum Message {
+    Hello,
+    Welcome {
+        version: u8,
+    },
+    Subscribe {
+        name: String,
+    },
+
+}
+
+fn main() {
     println!("Hello, world!");
 
-    if let Ok(mut stream) = TcpStream::connect("127.0.0.1:7878") {
-        println!("Connected to the server!");
 
-        let msg = "\"Hello\"".to_string();
+    match TcpStream::connect("127.0.0.1:7878") {
+        Ok(mut stream) => {
+            stream.set_nonblocking(true);
+            let message = Message::Hello;
+            send_message(&stream, message);
+            receive_messages(&stream);
+        },
+        Err(_) => panic!("Could not connect to server "),
+    }
 
-        let bytes_msg = msg.as_bytes();
-        let size = (bytes_msg.len() as u32).to_be_bytes();
+}
 
-        let result = stream.write(&[&size, bytes_msg].concat())?;
-        println!("Write result : {:?}, message: {}", result, msg);
+fn receive_messages(mut stream: &TcpStream){
+    loop {
+        let mut v = Vec::<u8>::new();
+        stream.read_to_end(&mut v);
+        let str = String::from_utf8_lossy(&v);
+        if str != "" {
+            println!("{str:?}");
+        }
+    }
+}
 
-/*
-        let mut buf_size = [0; 4];
-        stream.try_clone()?.read(&mut buf_size).expect("Could not read stream message size");
 
-        println!("{}", String::from_utf8_lossy(&buf_size));
-*/
-        stream.flush()?;
-        Ok(())
-    } else {
-        println!("Couldn't connect to server...");
-        Ok(())
+fn send_message(mut stream: &TcpStream, message: Message) {
+    if let Ok(message) = serde_json::to_string(&message) {
+        let bytes_message = message.as_bytes();
+        let message_size = bytes_message.len() as u32;
+        let message_length_as_bytes = message_size.to_be_bytes();
+        stream.write(&message_length_as_bytes).unwrap();
+        let result = stream.write(bytes_message).unwrap();
+        println!("result : {}, message: {}", result, message);
     }
 }
