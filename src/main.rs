ca4b8f5d34faa98f52350ebe222c755cfd8324dd
Nathan-dev-dot <maerna1@gmail.com>
2022-11-24 21:10:22 +0100

    recive publicLeaderBoard

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,18 @@ extern crate core;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use serde::{Deserialize, Serialize};
+use rand::Rng;
+use crate::structs::Message;
 
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub enum Message {
-    Hello,
-    Welcome {
-        version: u8,
-    },
-    Subscribe {
-        name: String,
-    },
-
-}
+mod structs;
 
 fn main() {
-    println!("Hello, world!");
-
-
     match TcpStream::connect("127.0.0.1:7878") {
         Ok(mut stream) => {
-            stream.set_nonblocking(true);
             let message = Message::Hello;
-            send_message(&stream, message);
-            let message = Message::Subscribe { name: "jean-pierre".to_string()};
             send_message(&stream, message);
 
             receive_messages(&stream);
-
         },
         Err(_) => panic!("Could not connect to server "),
     }
@@ -38,17 +22,43 @@ fn main() {
 
 fn receive_messages(mut stream: &TcpStream){
     loop {
-        let mut v = Vec::<u8>::new();
+        let mut buf_size = [0; 4];
+        stream.read(&mut buf_size);
+        let res_size = u32::from_be_bytes(buf_size);
+        if res_size == 0 {
+            continue
+        }
 
-        stream.read_to_end(&mut v);
+        let mut buf = vec![0; res_size as usize];
+        stream.read(&mut buf);
+        let string_receive = String::from_utf8_lossy(&buf);
 
-        let str = String::from_utf8_lossy(&v);
-        if str != "" {
-            println!("{str:?}");
+        match serde_json::from_str(&string_receive) {
+            Ok(message) => dispatch_messages(stream, message),
+            Err(_) => println!("Error while parsing message"),
         }
     }
 }
 
+fn dispatch_messages(mut stream: &TcpStream, message: Message) {
+    println!("Dispatching: {:?}", message);
+    match message {
+        Message::Welcome { version } => {
+            let mut rng = rand::thread_rng();
+            let n1: u8 = rng.gen();
+
+            let answer = Message::Subscribe { name: "Player".to_string() + &n1.to_string() };
+            send_message(&stream, answer);
+        }
+        Message::SubscribeResult(subscribeResult) => {
+            println!("SubscribeResult")
+        }
+        Message::PublicLeaderBoard( publicLeaderBoard ) => {
+            println!("{:?}", publicLeaderBoard);
+        }
+        _ => {print!("Error")}
+    }
+}
 
 fn send_message(mut stream: &TcpStream, message: Message) {
     if let Ok(message) = serde_json::to_string(&message) {
@@ -58,7 +68,6 @@ fn send_message(mut stream: &TcpStream, message: Message) {
         let message_length_as_bytes = message_size.to_be_bytes();
         stream.write(&message_length_as_bytes).unwrap();
 
-        let result = stream.write(bytes_message).unwrap();
-        println!("result : {}, message: {}", result, message);
+        stream.write(bytes_message).unwrap();
     }
 }
---
+++ src/structs.rs
@@ -0,0 +1,38 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum SubscribeError {
+    AlreadyRegistered,
+    InvalidName,
+}
+
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum SubscribeResult {
+    Ok,
+    Err (SubscribeError),
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct PublicPlayer{
+    name: String,
+    stream_id: String,
+    score: i32,
+    steps: u32,
+    is_active: bool,
+    total_used_time: f64
+}
+
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum Message {
+    Hello,
+    Welcome {
+        version: u8,
+    },
+    Subscribe {
+        name: String,
+    },
+    SubscribeResult (SubscribeResult),
+    PublicLeaderBoard (Vec<PublicPlayer>)
+}
