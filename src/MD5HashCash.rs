5630f02144145d065ef3309abb8f0aed5c72405a
Nathan-dev-dot <maerna1@gmail.com>
2022-11-29 17:39:35 +0100

    hash and concat message to seed

--- src/MD5HashCash.rs
+++ src/MD5HashCash.rs
@@ -3,6 +3,8 @@ use crate::structs::MD5HashCashOutput;
 use crate::structs::ChallengeAnswer;
 use crate::structs::ChallengeResolve;
 use crate::structs::MD5HashCashInput;
+use md5::{Md5, Digest};
+use hex_literal::hex;
 
 pub struct MD5HashCashResolver {
     pub input: MD5HashCashInput
@@ -24,7 +26,17 @@ impl ChallengeResolve for MD5HashCashResolver{
     }
 
     fn solve(&self) -> Self::Output {
-        println!("{}", self.input.complexity);
+        println!("{} {}", self.input.complexity, self.input.message.len());
+
+        let seed: &str = "0000000000000034C";
+        let msg: String = seed.to_owned() + self.input.message.as_str();
+        println!("{}", msg);
+
+        let mut hasher = Md5::new();
+        hasher.update(msg.into_bytes());
+        let result = hasher.finalize();
+        println!("{:?}", result.to_vec());
+
         MD5HashCashOutput{
             seed: 0,
             hashcode: "".to_string(),
